07d784467c9d10a4fa99656994649aa578c789f3
Chaskilon34 <jp.lozanoherrera@gmail.com>
2026-08-26 15:29:39 -0500

    Ejercuicio 3  biblioteca

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,75 @@
 package org.example;
-
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+/// se crea el array
+        Libro[] libros = new Libro[5];
 
-        Cliente cliente = new Cliente();
-        Producto producto1 = new Producto();
-        Venta venta = new Venta();
+/// Ingresar  datos sobre los libros
+        for (int i = 0; i < libros.length; i++) {
 
-        System.out.print("Indique su nombre: ");
-        String user = scan.next();
+            System.out.println("Libro " + (i + 1));
 
-        System.out.print("Nombre del producto: ");
-        String producto = scan.next();
+            System.out.print("Ingrese el titulo: ");
+            String titulo = sc.nextLine();
 
-        System.out.print("Precio del producto: ");
-        int precio = scan.nextInt();
+            System.out.print("Ingrese el autor: ");
+            String autor = sc.nextLine();
 
-        System.out.print("Indique la cantidad de objetos a pagar: ");
-        int cantidad = scan.nextInt();
+            System.out.print("Ingrese el año de publicacion: ");
+            int anio = sc.nextInt();
 
-        cliente.user = user;
-        producto1.nombre = producto;
-        producto1.precio = precio;
+            sc.nextLine();
 
-        int total = venta.calcularT(producto1.precio, cantidad);
+/// por aca crea el obgeto usando el contructor
+            libros[i] = new Libro(titulo, autor, anio);
+        }
 
-        System.out.println("Cliente: " + cliente.user);
-        System.out.println("Producto: " + producto1.nombre);
-        System.out.println("Precio: $" + producto1.precio);
-        System.out.println("Cantidad: " + cantidad);
-        System.out.println("Total a pagar: " + total);
+        System.out.println("\n--- LIBROS ---");
 
-        scan.close();
+      for (int i = 0; i < libros.length; i++) {
+            libros[i].mostrarInformacion();
+            System.out.println();
+        }
+///Busca el libro por us tituli
+      System.out.print("Ingrese el titulo del libro que desea buscar: ");
+        String tituloBuscar = sc.nextLine();
+
+      boolean encontrado = false;
+
+        for (int i = 0; i < libros.length; i++) {
+
+            if (libros[i].getTitulo().equalsIgnoreCase(tituloBuscar)) {
+             System.out.println("\nLibro encontrado:");
+             libros[i].mostrarInformacion();
+             encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("El libro no existe.");
+        }
+
+ ///aca busca el libro mas antiguo
+        Libro libroAntiguo = libros[0];
+
+        for (int i = 1; i < libros.length; i++) {
+
+            if (libros[i].getAnioPublicacion()
+                    < libroAntiguo.getAnioPublicacion()) {
+
+            libroAntiguo = libros[i];
+            }
+        }
+
+
+        System.out.println("\n LIBRO MAS ANTIGUO-----");
+
+        libroAntiguo.mostrarInformacion();
+        sc.close();
     }
 }
+
