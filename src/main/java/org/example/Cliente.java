32c001037798d6cfbe379d69d982ee8f9d52c878
Chaskilon34 <jp.lozanoherrera@gmail.com>
2026-08-26 14:23:03 -0500

    2 Ejercicio_Java

---
+++ src/main/java/org/example/Cliente.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Cliente {
+    String user ;
+
+    public void mostrarCliente() {
+        System.out.println("Cliente" + user );
+    }
+}
+
---
+++ src/main/java/org/example/Venta.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Venta {
+
+    public  int calcularT(int precio , int cantidad){
+        return precio * cantidad;
+    }
+}
